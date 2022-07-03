a7a2ae90676760a6d9745a73538fbd0f424fe4a5
yangjinghua <1692047690@qq.com>
2022-07-03 23:16:17 +0800

    更新了房屋详情的页面代码和筛选房屋的过滤器

--- lib/application.dart
+++ lib/application.dart
@@ -3,19 +3,24 @@ import 'package:flutter/material.dart';
 import 'package:hook_up_rent/home/index.dart';
 import 'package:hook_up_rent/login.dart';
 import 'package:hook_up_rent/routes.dart';
+import 'package:hook_up_rent/scoped_model/room_filter.dart';
+import 'package:scoped_model/scoped_model.dart';
 class Application extends StatelessWidget {
   const Application({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     FluroRouter router = FluroRouter();
     Routes.configureRoutes(router);
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.green),
-      home: Scaffold(
-        body: HomePage(),
+    return ScopedModel(
+      model: FilterBarModel(),
+      child: MaterialApp(
+        theme: ThemeData(primarySwatch: Colors.green),
+        home: Scaffold(
+          body: HomePage(),
+        ),
+        debugShowCheckedModeBanner: false,
+        onGenerateRoute: router.generator,
       ),
-      debugShowCheckedModeBanner: false,
-      onGenerateRoute: router.generator,
     );
   }
 }
